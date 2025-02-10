134d12b420b87bb1a27044eaf939141a4e545053
Bozhidar Dimitrov <bozhidar.dimitrov@gmail.com>
2025-02-10 14:17:57 +0200

    Adding changes

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void) {
   printf("!Hello, World!\n");
   printf("This is remotely added line\n");
 
-  printf("Adding another line of code \n"):
+  printf("Adding another line of code \n");
 
   return 0;
 }
